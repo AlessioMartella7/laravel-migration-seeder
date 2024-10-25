8eaee2459f6e518734e6630ef462fde49970c755
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-25 15:47:39 +0200

    created trains table migration

---
+++ database/migrations/2024_10_25_131915_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+
+            $table->id();
+            $table->string('company',100);
+            $table->string('from', 100);
+            $table->string('to', 100);
+            $table->string('train_code',50);
+            $table->tinyInteger('carriage_number')->unsigned();
+            $table->boolean('on_time')->default(true);
+            $table->boolean('canceled')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
