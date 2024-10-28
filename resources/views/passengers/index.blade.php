5fd35e5d4720246d32095d56de37d227e374ef3c
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-28 15:04:08 +0100

    rendered table in passengers.index view

--- resources/views/passengers/index.blade.php
+++ resources/views/passengers/index.blade.php
@@ -1,5 +1,41 @@
 @extends('layouts.app')
 @section('page-title', 'Passenger')
 @section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center my-5 text-primary">
+                    Passengers
+                </h1>
+            </div>
+            <div class="col 12">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr class="table-primary">
+                            <th scope="col">Name</th>
+                            <th scope="col">Surname</th>
+                            <th scope="col">Age</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            @forelse ($passengers as $passenger)
+                        <tr>
+                            <td>{{ $passenger->name }}</td>
+                            <td>{{ $passenger->surname }}</td>
+                            <td>{{ $passenger->age }}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="11" class="text-center text-danger">No trains available.</td>
+                        </tr>
+                        @endforelse
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 @endsection
