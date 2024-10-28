fb8dc0e12d97b4d357d882eccfb93b3add119ecc
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-28 16:39:24 +0100

    added DelayedTrainSeeder

--- database/seeders/DelayedTrainSeeder.php
+++ database/seeders/DelayedTrainSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\DelayedTrain;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,51 @@ class DelayedTrainSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $delayedTrains =[
+            [
+                'company' => 'Trenitalia',
+                'from' => 'Roma',
+                'to' => 'Milano',
+                'train_code' => 'FD5412',
+                'departure' => '19:10:46',
+                'arrival' => '02:10:10',
+                'carriage_number' => '22',
+
+            ],
+            [
+                'company' => 'Italo',
+                'from' => 'Livorno',
+                'to' => 'Bologona',
+                'train_code' => 'FF5912',
+                'departure' => '02:10:46',
+                'arrival' => '05:10:19',
+                'carriage_number' => '12',
+
+            ],
+            [
+                'company' => 'Speedotrain',
+                'from' => 'Lecce',
+                'to' => 'Genova',
+                'train_code' => 'AD5282',
+                'departure' => '10:10:46',
+                'arrival' => '23:30:10',
+                'carriage_number' => '28',
+
+            ],
+        ];
+
+        foreach ($delayedTrains as $delayedTrain) {
+            $newDelayedTrain = new DelayedTrain();
+            $newDelayedTrain->company = $delayedTrain['company'];
+            $newDelayedTrain->from = $delayedTrain['from'];
+            $newDelayedTrain->to = $delayedTrain['to'];
+            $newDelayedTrain->train_code = $delayedTrain['train_code'];
+            $newDelayedTrain->departure = $delayedTrain['departure'];
+            $newDelayedTrain->arrival = $delayedTrain['arrival'];
+            $newDelayedTrain->carriage_number = $delayedTrain['carriage_number'];
+            $newDelayedTrain->save();
+        }
     }
 }
+
+
